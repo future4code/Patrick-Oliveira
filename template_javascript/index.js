49248efbe578944e62efb149f9a1f7d2ccc62115
Patrick Oliveira <patrick.eduardo.oliveira.alves@hotmail.com>
2021-05-25 15:59:31 -0300

    Adicionando as tarefas da aula04

---
+++ template_javascript/index.js
@@ -0,0 +1,132 @@
+/*
+
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", false)
+
+
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", false) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", false)
+
+console.log("d. ", typeof Number)
+
+*/
+
+
+/***************************/
+
+
+/*
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+*/
+
+//Será impresso a concatenação dos dois valores. 
+
+//console.log(soma)
+
+/***************************/
+
+/*
+
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+
+*/
+
+/***************************/
+
+/*
+
+let suaIdade = Number(prompt("Qual sua idade?"));
+let idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo(a)?"));
+
+let maiorIdade = suaIdade > idadeAmigo
+let diferencaIdade = suaIdade % idadeAmigo
+
+console.log(maiorIdade)
+
+console.log(diferencaIdade)
+
+*/
+
+/***************************/
+
+
+/*
+
+let numeroPar = Number(prompt("Insira aqui um número par:"));
+
+let restoDivisao = numeroPar % 2;
+
+console.log(restoDivisao);
+
+*/
+
+
+//Todos os resultados dão 0 caso seja um número par
+//Todos os resultados dão 1 caso seja um número impar
+
+/***************************/
+
+/*
+let idade = Number(prompt('Qual sua idade?'))
+
+let idadeHoras = idade * 8760;
+let idadeDias =  idade * 365;
+let idadeMeses = idade * 12;
+
+
+console.log("Idade em horas:", idadeHoras)
+console.log("Idade em dais:", idadeDias)
+console.log("Idade em meses:", idadeMeses)
+*/
+
+
+
+/***************************/
+
+/*
+
+let primeiroNumero = Number(prompt('Digite aqui um número'));
+let segundoNumero = Number(prompt('Digite aqui o segundo número'));
+
+const numeroDivisivel1 = primeiroNumero % segundoNumero === 0;
+const numeroDivisivel2 = segundoNumero % primeiroNumero === 0;
+
+console.log("O primeiro numero é maior que segundo?", primeiroNumero > segundoNumero);
+console.log("O primeiro numero é igual ao segundo?", primeiroNumero == segundoNumero);
+console.log("O primeiro numero é divisível pelo segundo?", numeroDivisivel1);
+console.log("O segundo numero é divisível pelo primeiro?", numeroDivisivel2);
+
+*/
+
+/*************DESAFIOS**************/
+
+/*
+let custoEnergia = 0.05;
+let quilowatts = Number(prompt("Qual foi seu consumo de energia até o dia de hoje?"));
+
+let custoMes = custoEnergia * quilowatts;
+
+let custoMesDesconto = custoMes * 0.85;
+
+console.log("Valor normal:", custoMes);
+console.log("Valor com desconto de 15%:", custoMesDesconto);
+*/
+
